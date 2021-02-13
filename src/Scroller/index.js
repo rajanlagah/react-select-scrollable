ff82357b110b2fe3b6787fc1f344ede3700f93dd
rajanlagah <rajanlagah@gmail.com>
2021-02-14 01:14:18 +0530

    🚧  feat : Modal selection option

--- src/Scroller/index.js
+++ src/Scroller/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+
 
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
@@ -17,10 +17,14 @@ export default class Scroller extends Component {
 			scrollTolrence: 0,
 			totalData: [1, 2, 3, 4, 5, 6],
 			centerElementPossition: 0,
-			visibleData: 1
+			visibleData: 1,
+			centerData: 20,
+			closeModal: true
 		}
 
 		this.handleScroll = this.handleScroll.bind(this)
+		this.handleItemClick = this.handleItemClick.bind(this)
+		this.toggleModal = this.toggleModal.bind(this)
 	}
 	componentDidMount() {
 		const { scrollTolrence } = { ...this.props }
@@ -35,57 +39,92 @@ export default class Scroller extends Component {
 		}
 	}
 
-	getClosestElementToCenter = () => {
-		let closestDiff = Math.pow(2,10)
-		let closestNode = null
-		
-		const elements = document.querySelectorAll('.elemets')
-		const centerElement = document.getElementById('center-element-display')
-		
-		console.log(elements[0].getBoundingClientRect())
+	// getClosestElementToCenter = (centerData) => {
+	// 	let closestDiff = Math.pow(2, 10)
+	// 	let closestNode = null
 
-		elements.forEach(element => {
-			const diff = Math.abs(element.getBoundingClientRect().y - centerElement.getBoundingClientRect().y) 
-			if(diff < closestDiff){
-				console.log("setting up")
-				closestDiff = diff 
-				closestNode = element
-			}
-		});
+	// 	const elements = document.querySelectorAll('.elemets')
+	// 	const centerElement = document.getElementById('center-element-display')
 
-		return closestNode
-	}
+	// 	// console.log(elements[0].getBoundingClientRect())
+
+	// 	elements.forEach(element => {
+	// 		const diff = Math.abs(element.getBoundingClientRect().y - centerData)
+	// 		if (diff < closestDiff) {
+	// 			closestNode = element
+	// 		}
+	// 	});
+
+	// 	return closestNode
+	// }
 
 	handleScroll() {
-		const { visibleData } = { ...this.state }
+		return
+
+		// const { visibleData, centerData } = { ...this.state }
+		// const { onSelectedDataChange } = this.props
+
+		// const nodeAtCenter = this.getClosestElementToCenter(centerData)
+		// const intersectValue = nodeAtCenter.getAttribute("value")
+		// if (visibleData != intersectValue) {
+		// 	onSelectedDataChange(intersectValue)
+		// }
+		// this.setState({
+		// 	visibleData: intersectValue
+		// })
+	}
+
+	handleItemClick(e) {
 		const { onSelectedDataChange } = this.props
-		
-		const nodeAtCenter = this.getClosestElementToCenter()
-		const intersectValue = nodeAtCenter.getAttribute("value")
-		if( visibleData != intersectValue){
-			onSelectedDataChange(intersectValue)
-		}
+
+		const selectedValue = e.target.getAttribute('value')
+
+		onSelectedDataChange(selectedValue)
 		this.setState({
-			visibleData:intersectValue
+			visibleData: selectedValue,
+			closeModal: true
+		})
+	}
+
+	toggleModal(e) {
+		const { closeModal } = { ...this.state }
+		this.setState({
+			closeModal: !closeModal
 		})
 	}
 
 	render() {
-		let { scrollPos, isMovingUp, visibleData } = { ...this.state }
 		const { data } = { ...this.props }
+		const { closeModal,visibleData } = { ...this.state }
+
+		if (closeModal) {
+			return (
+				<div>
+					<select value={visibleData} onChange={this.handleChange} onClick={() => this.setState({ closeModal: false })}>
+						{data && data.map((item, index) => <option value={item.value}>{item.displayName}</option>)}
+					</select>
+
+				</div>
+
+			)
+		}
 
 		return (
-			<div className={styles.outterContiner}>
-				<div className={styles.optionsContiner} id='modal' onScroll={this.handleScroll}>
-					<li className={styles.listOfOptions}>
-						{data && data.map((item, index) => <ul><a className="elemets" href='#' value={item} >{item}</a></ul>)}
-
-					</li>
-				</div>
+			<div>
+				<select value={visibleData} disabled />
 				<div>
-					<span className={styles.selectedElement} id="center-element-display"> {visibleData}</span>
+					<div className={`${styles.inputModal}  ${styles.outterContiner}`}>
+						<div className={styles.optionsContiner} id='modal' onScroll={this.handleScroll}>
+							<span className={styles.listOfOptions}>
+								{data && data.map((item, index) => <a className="elemets" href='#' value={item.value} onClick={this.handleItemClick}>{item.displayName}</a>)}
+							</span>
+						</div>
+						<button onClick={this.toggleModal} className={`${styles.modalCloseButton}`}>cancel</button>
+					</div>
 				</div>
 			</div>
+
 		)
+
 	}
 }
